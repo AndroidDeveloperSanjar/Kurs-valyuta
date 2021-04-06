5d0427bbc3fe8ce9c88982f7a224e9ac3cb4189f
Sanjar Karimov <sanjarkarimov1996@mail.ru>
2021-04-06 19:50:20 +0500

    Created main adapter!

--- app/src/main/java/uz/androbeck/kursvalyuta/MainActivity.kt
+++ app/src/main/java/uz/androbeck/kursvalyuta/MainActivity.kt
@@ -2,16 +2,48 @@ package uz.androbeck.kursvalyuta
 
 import android.os.Bundle
 import android.text.Html
+import android.viewbinding.library.activity.viewBinding
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import uz.androbeck.kursvalyuta.adapter.MainAdapter
+import uz.androbeck.kursvalyuta.adapter.model.MainModel
+import uz.androbeck.kursvalyuta.databinding.ActivityMainBinding
+import uz.androbeck.kursvalyuta.utils.Helper
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MainAdapter.MainAdapterListener {
+
+    private lateinit var mainAdapter: MainAdapter
+
+    private val binding: ActivityMainBinding by viewBinding()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
-            supportActionBar?.setTitle(Html.fromHtml("<font color='#546E7A'>Kurs valyuta</font/g_medium>", 1))
+            supportActionBar?.setTitle(
+                Html.fromHtml(
+                    "<font color='#546E7A'>Kurs valyuta</font/g_medium>",
+                    1
+                )
+            )
         } else {
             supportActionBar?.setTitle(Html.fromHtml("<font color='#546E7A'>Kurs valyuta</font/g_medium>"))
         }
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        initRV()
+    }
+
+    private fun initRV() {
+        mainAdapter = MainAdapter(this)
+        with(binding) {
+            rv.layoutManager = LinearLayoutManager(this@MainActivity)
+            rv.adapter = mainAdapter
+            mainAdapter.submitList(Helper.dataList())
+        }
+    }
+
+    override fun clickCalculate(position: Int, data: MainModel) {
+        Toast.makeText(this, "Kalkulyator bosildi position -> #$position", Toast.LENGTH_SHORT)
+            .show()
     }
 }
--- app/src/main/java/uz/androbeck/kursvalyuta/MainAdapter.kt
+++
@@ -1,4 +0,0 @@
-package uz.androbeck.kursvalyuta
-
-class MainAdapter {
-}
